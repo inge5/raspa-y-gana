7347346831d220cf4886301f276f62f2fc45b67e
Nelson Zambrano <inge5neuromedia@gmail.com>
2022-01-20 12:35:52 -0500

    terminos actualizados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,30 +6,52 @@ import { PeticionesService } from 'src/app/services/peticiones.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   formulario: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder, private router: Router, private peticiones: PeticionesService) { 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private peticiones: PeticionesService
+  ) {}
 
-  }
-
-  crearFormulario(){
+  crearFormulario() {
     this.formulario = this.fb.group({
       name: ['', Validators.required],
-      document: ['', [Validators.required, Validators.min(100000), Validators.max(9999999999)]],
+      document: [
+        '',
+        [
+          Validators.required,
+          Validators.min(100000),
+          Validators.max(9999999999),
+        ],
+      ],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.min(1000000), Validators.max(9999999999)]],
-      code: ['', [Validators.required, Validators.min(10000000000000000000), Validators.max(99999999999999999999)]],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.min(1000000),
+          Validators.max(9999999999),
+        ],
+      ],
+      code: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(20),
+        ],
+      ],
       points: [''],
-      acepto: [false, Validators.required]
-    })
+      acepto: [false, Validators.required],
+    });
   }
 
   ngOnInit(): void {
-    if(sessionStorage.getItem('user')){
+    if (sessionStorage.getItem('user')) {
       sessionStorage.removeItem('user');
       sessionStorage.removeItem('puntos');
       this.peticiones.consultarDatosLocales();
@@ -37,76 +59,94 @@ export class HomeComponent implements OnInit {
     this.crearFormulario();
   }
 
-  get Name(){
+  get Name() {
     return this.formulario.get('name');
   }
-  get Document(){
+  get Document() {
     return this.formulario.get('document');
   }
-  get Email(){
+  get Email() {
     return this.formulario.get('email');
   }
-  get Phone(){
+  get Phone() {
     return this.formulario.get('phone');
   }
-  get Code(){
+  get Code() {
     return this.formulario.get('code');
   }
 
-  get validarName(){
-    return this.formulario.get('name')?.invalid && (this.formulario.get('name')?.dirty || this.formulario.get('name')?.touched);
+  get validarName() {
+    return (
+      this.formulario.get('name')?.invalid &&
+      (this.formulario.get('name')?.dirty ||
+        this.formulario.get('name')?.touched)
+    );
   }
-  get validarDocument(){
-    return this.formulario.get('document')?.invalid && (this.formulario.get('document')?.dirty || this.formulario.get('document')?.touched);
+  get validarDocument() {
+    return (
+      this.formulario.get('document')?.invalid &&
+      (this.formulario.get('document')?.dirty ||
+        this.formulario.get('document')?.touched)
+    );
   }
-  get validarEmail(){
-    return this.formulario.get('email')?.invalid && (this.formulario.get('email')?.dirty || this.formulario.get('email')?.touched);
+  get validarEmail() {
+    return (
+      this.formulario.get('email')?.invalid &&
+      (this.formulario.get('email')?.dirty ||
+        this.formulario.get('email')?.touched)
+    );
   }
-  get validarPhone(){
-    return this.formulario.get('phone')?.invalid && (this.formulario.get('phone')?.dirty || this.formulario.get('phone')?.touched);
+  get validarPhone() {
+    return (
+      this.formulario.get('phone')?.invalid &&
+      (this.formulario.get('phone')?.dirty ||
+        this.formulario.get('phone')?.touched)
+    );
   }
-  get validarCode(){
-    return this.formulario.get('code')?.invalid && (this.formulario.get('code')?.dirty || this.formulario.get('code')?.touched);
+  get validarCode() {
+    return (
+      this.formulario.get('code')?.invalid &&
+      (this.formulario.get('code')?.dirty ||
+        this.formulario.get('code')?.touched)
+    );
   }
 
-  get correctoName(){
+  get correctoName() {
     return this.Name?.valid;
   }
-  get correctoDocument(){
+  get correctoDocument() {
     return this.Document?.valid;
   }
-  get correctoEmail(){
+  get correctoEmail() {
     return this.Email?.valid;
   }
-  get correctoPhone(){
+  get correctoPhone() {
     return this.Phone?.valid;
   }
-  get correctoCode(){
+  get correctoCode() {
     return this.Code?.valid;
   }
 
-
-  PasarRuleta(){
-    console.log(this.formulario);
-    if(this.formulario.invalid && !this.formulario.get('acepto')!.value){
-      return Object.values(this.formulario.controls).forEach(control => {
+  PasarRuleta() {
+    if (this.formulario.invalid && !this.formulario.get('acepto')!.value) {
+      return Object.values(this.formulario.controls).forEach((control) => {
         control.markAsTouched();
-      })
+      });
     }
-    if(this.formulario.invalid && this.formulario.get('acepto')!.value){
-      return Object.values(this.formulario.controls).forEach(control => {
+    if (this.formulario.invalid && this.formulario.get('acepto')!.value) {
+      return Object.values(this.formulario.controls).forEach((control) => {
         control.markAsTouched();
-      })
+      });
     }
-    if(!this.formulario.get('acepto')!.value){
+    if (!this.formulario.get('acepto')!.value) {
       alert('Debes aceptar terminos y condiciones');
       return;
     }
-    let phone:number = this.Phone!.value
-    let code:number = this.Code!.value;
-    this.formulario.controls['phone'].setValue(phone.toString())
-    this.formulario.controls['code'].setValue(code.toString());
-    sessionStorage.setItem('user', JSON.stringify(this.formulario.value))
+    let phone: number = this.Phone!.value;
+    let code: number = this.Code!.value;
+    this.formulario.controls['phone'].setValue(phone.toString());
+    this.formulario.controls['code'];
+    sessionStorage.setItem('user', JSON.stringify(this.formulario.value));
     this.router.navigateByUrl('/ruleta');
   }
 }
